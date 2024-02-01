141980a46c568e77f908e5cc8cc696baced83139
tristannottelman <trno@live.nl>
2024-02-01 15:04:52 +0100

    Fixed minor issue

--- main/secrets.c
+++ main/secrets.c
@@ -6,43 +6,43 @@ uint8_t BLOB[256];
 
 void set_device(uint8_t device) {
 	if (device == 1) {
-		// Add the secret keys here
-		uint8_t mac[6] = {};
+		// Add the secret keys here as byte array
+		uint8_t mac[0] = {};
 		uint8_t device_key[16] = {};
 		uint8_t blob[256] = {};
 		for (int i = 0; i < 6; i = i + 1) { MAC[i] = mac[i]; }
 		for (int i = 0; i < 16; i = i + 1) { DEVICE_KEY[i] = device_key[i]; }
 		for (int i = 0; i < 256; i = i + 1) { BLOB[i] = blob[i]; }
 	} else if (device == 2) {
-		uint8_t mac[6] = {};
+		uint8_t mac[1] = {};
 		uint8_t device_key[16] = {};
 		uint8_t blob[256] = {};
 		for (int i = 0; i < 6; i = i + 1) { MAC[i] = mac[i]; }
 		for (int i = 0; i < 16; i = i + 1) { DEVICE_KEY[i] = device_key[i]; }
 		for (int i = 0; i < 256; i = i + 1) { BLOB[i] = blob[i]; }
 	} else if (device == 3) {
-		uint8_t mac[6] = {};
+		uint8_t mac[2] = {};
 		uint8_t device_key[16] = {};
 		uint8_t blob[256] = {};
 		for (int i = 0; i < 6; i = i + 1) { MAC[i] = mac[i]; }
 		for (int i = 0; i < 16; i = i + 1) { DEVICE_KEY[i] = device_key[i]; }
 		for (int i = 0; i < 256; i = i + 1) { BLOB[i] = blob[i]; }
 	} else if (device == 4) {
-		uint8_t mac[6] = {};
+		uint8_t mac[3] = {};
 		uint8_t device_key[16] = {};
 		uint8_t blob[256] = {};
 		for (int i = 0; i < 6; i = i + 1) { MAC[i] = mac[i]; }
 		for (int i = 0; i < 16; i = i + 1) { DEVICE_KEY[i] = device_key[i]; }
 		for (int i = 0; i < 256; i = i + 1) { BLOB[i] = blob[i]; }
 	} else if (device == 5) {
-		uint8_t mac[6] = {};
+		uint8_t mac[4] = {};
 		uint8_t device_key[16] = {};
 		uint8_t blob[256] = {};
 		for (int i = 0; i < 6; i = i + 1) { MAC[i] = mac[i]; }
 		for (int i = 0; i < 16; i = i + 1) { DEVICE_KEY[i] = device_key[i]; }
 		for (int i = 0; i < 256; i = i + 1) { BLOB[i] = blob[i]; }
 	} else {
-		uint8_t mac[6] = {};
+		uint8_t mac[5] = {};
 		uint8_t device_key[16] = {};
 		uint8_t blob[256] = {};
 		for (int i = 0; i < 6; i = i + 1) { MAC[i] = mac[i]; }
